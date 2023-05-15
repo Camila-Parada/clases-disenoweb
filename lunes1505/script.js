12d82d541444db8675670bac021f2de45f1d0ad4
Camila-Parada <128399287+Camila-Parada@users.noreply.github.com>
2023-05-15 12:15:56 -0400

    Update script.js

--- lunes1505/script.js
+++ lunes1505/script.js
@@ -1,13 +1,11 @@
 console.log ("Busco mantenerme tranquila");
-
-console.error ("Me siento sola");
+// console.error ("Me siento sola")
 
 let botonInicio = document.getElementById("inicio")/* .addEventListener("click", displayDate); */
 console.log(botonInicio);
 
 /* function displayDate () {
-    document.getElementById("inicio").innerHTML = Date ();
-} */
+    document.getElementById("inicio").innerHTML = Date ();} */
 
 botonInicio.addEventListener ("click", cambiarColor)
 function cambiarColor () {
@@ -19,7 +17,7 @@ function cambiarMouse () {
     console.log ("poto");
     botonInicio.style.backgroundColor = ("blue");
 /*     botonInicio.style.font = ("Times New Roman");
- */    botonInicio.style.fontSize = ("20px");
+ */    botonInicio.style.fontSize = ("20px")
 }
 
 // Función sacada de WW3, versión script (abajo)
@@ -28,3 +26,16 @@ function bacan() {
     var x = document.getElementById("myInput").value;
     document.getElementById("demo").innerHTML = "Estás tecleando:" + x;
   }
+
+//   Nuevo contenido, imprimir cosas ocultas
+
+let formulario = document.getElementById("formulario");
+console.log (formulario);
+formulario.addEventListener ("submit", grabarInfo);
+function grabarInfo(){
+    console.log("ahora");
+}
+
+// Hay que considerar los tipos de elementos usados en HTML. 
+// el valor de tipo "input" (HTML) incluido dentro del formulario (form) y 
+// el cual se expresa como un boton no reconoce en js la funcion click,
